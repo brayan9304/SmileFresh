712a70016f21009e30641b95062303087f855026
brayan9304 <brayan-416@hotmail.com>
2017-05-07 15:22:37 -0500

    full add date

--- src/app/components/calendar/event-date.ts
+++ src/app/components/calendar/event-date.ts
@@ -1,10 +1,10 @@
 
 export class EventCalendar{
-  //date:string;
-  //startTime:string;
-  //endTime:string;
-  //doctor:string;
-  //price:string;
+  date:string;
+  startTime:string;
+  endTime:string;
+  doctor:string;
+  price:string;
   patient:string;
-  start:string;
+
 }
